033a7f98cad6f44c3c954ee430e964efcd212945
hiddenblue <273570606@qq.com>
2020-10-28 09:34:44 +0800

    修改后的正确代码

--- 两端向中间汇聚/两端向中间汇聚/两端向中间汇聚.c
+++ 两端向中间汇聚/两端向中间汇聚/两端向中间汇聚.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <windows.h>
 int main()
 {
 	char arr1[] = "Hello Hidden.Blue";
@@ -7,8 +9,10 @@ int main()
 	int left = 0;
 	int right = strlen(arr2)-1;
 	while (left <= right){
+		Sleep(1000);
+		system("cls");
 		arr2[left] = arr1[left];
-		arr2[right] = arr1[left];
+		arr2[right] = arr1[right];
 		left++;
 		right--;
 		printf("%s\n", arr2);
