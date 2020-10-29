7c600e5f5a57bc4fa8c76d9536871dcf79240c9a
hiddenblue <273570606@qq.com>
2020-10-29 10:24:00 +0800

    登录，上限三次

--- 三次登录/三次登录/三次登录.c
+++ 三次登录/三次登录/三次登录.c
@@ -1,10 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+
 int main()
 {
 	for (int i = 1; i <= 3; i++){
-		char password[20] = "159357";
+		char password[20] = "159957";
 		char input[20] = { 0 };
 		printf("ÇëÊäÈëÃÜÂë£º\n");
 		scanf("%s", &input);
